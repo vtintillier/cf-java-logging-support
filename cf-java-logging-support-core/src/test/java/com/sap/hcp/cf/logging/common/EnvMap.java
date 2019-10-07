ef9abf8436e6519acfcc8db4e79c56b2a2be9caf
Andreas Klein <andreas.klein@sap.com>
2019-10-07 14:19:27 +0200

    Add test for org id/name

--- cf-java-logging-support-core/src/test/java/com/sap/hcp/cf/logging/common/EnvMap.java
+++ cf-java-logging-support-core/src/test/java/com/sap/hcp/cf/logging/common/EnvMap.java
@@ -8,19 +8,38 @@ public class EnvMap {
     public static final String VCAP_APP_ID = "867b99ba-1caa-4d91-b2ee-82982234f58a";
     public static final String VCAP_SPACE_NAME = "test-space";
     public static final String VCAP_SPACE_ID = "69ca5b0f-cc66-4f8e-9d17-f102f60142e2";
+    public static final String VCAP_ORGANIZATION_NAME = "test-org";
+    public static final String VCAP_ORGANIZATION_ID = "6b8cf2f9-bf9a-45f6-8774-0a0f42cb27f3";
     public static final String VCAP_INSTANCE_IDX = "0";
-
-    public static final String VCAP_APPLICATION = "{\"limits\":{\"mem\":256,\"disk\":1024,\"fds\":16384}, " +
-                                                  "\"application_version\":\"a9efcfae-ee23-465f-9985-401099500825\"," +
-                                                  "\"application_name\":\"" + VCAP_APP_NAME + "\"," +
-                                                  "\"application_uris\":[\"test-app.acme.org\"],\"version\":\"a9efcfae-ee23-465f-9985-401099500825\",\"name\":\"test-app\"," +
-                                                  "\"space_name\":\"" + VCAP_SPACE_NAME + "\"," + "\"space_id\":\"" +
-                                                  VCAP_SPACE_ID + "\"," +
-                                                  "\"uris\":[\"test-app.acme.org\"],\"users\":null," +
-                                                  "\"application_id\":\"" + VCAP_APP_ID + "\"," +
-                                                  "\"instance_id\":\"3b671ef5fd86407cb8807f303824c870\"," +
-                                                  "\"instance_index\":\"" + VCAP_INSTANCE_IDX + "\"," +
-                                                  "\"host\":\"0.0.0.0\",\"port\":61999,\"started_at\":\"2015-03-17 12:07:49 +0000\",\"started_at_timestamp\":1426594069,\"start\":\"2015-03-17 12:07:49 +0000\",\"state_timestamp\":1426594069}";
+    public static final String VCAP_APPLICATION = "{" +
+                                                  "\"application_id\":\"" + VCAP_APP_ID + "\", " +
+                                                  "\"application_name\":\"" + VCAP_APP_NAME + "\", " +
+                                                  "\"application_uris\":[  " +
+                                                  "\"testapp.testdomain.com\" " +
+                                                  "], " +
+                                                  "\"application_version\":\"92720bd3-2735-489d-bf4b-e5bb05c752e7\", " +
+                                                  "\"cf_api\":\"https://api.cf.sap.hana.ondemand.com\", " +
+                                                  "\"host\":\"0.0.0.0\", " +
+                                                  "\"instance_id\":\"34b12c70-5443-426f-75c4-3260\", " +
+                                                  "\"instance_index\":\"" + VCAP_INSTANCE_IDX + "\", " +
+                                                  "\"limits\":{  " +
+                                                  "\"disk\":1024, " +
+                                                  "\"fds\":16384, " +
+                                                  "\"mem\":1024 " +
+                                                  "}, " +
+                                                  "\"name\":\"d048888-logging-sample-app\", " +
+                                                  "\"organization_id\":\"" + VCAP_ORGANIZATION_ID + "\", " +
+                                                  "\"organization_name\":\"" + VCAP_ORGANIZATION_NAME + "\", " +
+                                                  "\"port\":8080, " +
+                                                  "\"process_id\":\"a6026141-8d69-4e9d-b62d-8e1d982f7953\", " +
+                                                  "\"process_type\":\"web\", " +
+                                                  "\"space_id\":\"" + VCAP_SPACE_ID + "\", " +
+                                                  "\"space_name\":\"" +VCAP_SPACE_NAME + "\", " +
+                                                  "\"uris\":[  " +
+                                                  "\"testapp.testdomain.com\" " +
+                                                  "], " +
+                                                  "\"version\":\"92720bd3-2735-489d-bf4b-e5bb05c752e7\" " +
+                                                  "}";
 
     public static final String NOT_SET = "not-set";
 
