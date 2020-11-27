ef15bb407e11bf567be2532f6735aabdd02ded29
Karsten Schnitter <k.schnitter@sap.com>
2020-11-27 08:30:44 +0100

    Remove jaxb dependencies from production code
    These dependencies were introduced for Java11 compatibility.
    By switching to the Java8 Base64 implementation they can be
    eliminated. There is still a test-scoped dependency in the jersey
    module, that will not be removed.

--- cf-java-logging-support-servlet/src/main/java/com/sap/hcp/cf/logging/servlet/dynlog/PublicKeyReader.java
+++ cf-java-logging-support-servlet/src/main/java/com/sap/hcp/cf/logging/servlet/dynlog/PublicKeyReader.java
@@ -6,8 +6,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
-
-import javax.xml.bind.DatatypeConverter;
+import java.util.Base64;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +17,11 @@ public class PublicKeyReader {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(PublicKeyReader.class);
 
-    public RSAPublicKey readPublicKey(Environment environment) throws IOException, NoSuchAlgorithmException,
-                                                               InvalidKeySpecException {
+    private PublicKeyReader() {
+    }
+
+    public static RSAPublicKey readPublicKey(Environment environment) throws IOException, NoSuchAlgorithmException,
+                                                                      InvalidKeySpecException {
 
         String pemKey = environment.getVariable("DYN_LOG_LEVEL_KEY");
 
@@ -32,14 +34,11 @@ public class PublicKeyReader {
             pemKey = pemKey.replace("\r", "");
             pemKey = pemKey.replace("-----END PUBLIC KEY-----", "");
 
-            byte[] pubKeyBytes = DatatypeConverter.parseBase64Binary(pemKey);
+            byte[] pubKeyBytes = Base64.getDecoder().decode(pemKey);
 
             X509EncodedKeySpec spec = new X509EncodedKeySpec(pubKeyBytes);
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             return (RSAPublicKey) keyFactory.generatePublic(spec);
-
         }
-
     }
-
 }
