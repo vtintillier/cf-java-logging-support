fe314a1ecf3fc549e920258ff34a8c03ffcdf10d
Karsten Schnitter <k.schnitter@sap.com>
2021-06-25 16:16:37 +0200

    Switch Custom Metrics Shipment
    SAP BTP Application Logging has been changed to accept custom metrics within
    the log message stream. For this the messages need to be in JSON format and
    contain a field "type":"metrics" and an array of metrics in field "metrics". The prior
    approach using an Http endpoint is deprecated for removal. This commit changes
    cf-java-logging-support to provide custom metrics according to the new format by
    writing the correct JSON to `System.out`.

--- cf-java-monitoring-custom-metrics-clients/cf-custom-metrics-clients-core/src/main/java/com/sap/cloud/cf/monitoring/client/model/Metric.java
+++ cf-java-monitoring-custom-metrics-clients/cf-custom-metrics-clients-core/src/main/java/com/sap/cloud/cf/monitoring/client/model/Metric.java
@@ -74,4 +74,43 @@ public class Metric implements Serializable {
             .toString();
     }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((tags == null) ? 0 : tags.hashCode());
+		result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
+		long temp;
+		temp = Double.doubleToLongBits(value);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Metric other = (Metric) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (tags == null) {
+			if (other.tags != null)
+				return false;
+		} else if (!tags.equals(other.tags))
+			return false;
+		if (timestamp != other.timestamp)
+			return false;
+		if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+			return false;
+		return true;
+	}
+
 }
