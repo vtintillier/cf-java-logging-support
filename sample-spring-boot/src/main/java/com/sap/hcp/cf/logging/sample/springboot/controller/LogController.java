ab59d0f94b97281cbb7d2b72f07c1d61aa244799
Karsten Schnitter <k.schnitter@sap.com>
2021-07-26 21:57:10 +0200

    Instrument Spring Boot Sample with Custom Metrics
    This is a showcase on how to configure micrometer to send metrics as log messages.
    This shipping mechanism is supported by SAP Application Logging service for SAP BTP.

--- sample-spring-boot/src/main/java/com/sap/hcp/cf/logging/sample/springboot/controller/LogController.java
+++ sample-spring-boot/src/main/java/com/sap/hcp/cf/logging/sample/springboot/controller/LogController.java
@@ -2,11 +2,15 @@ package com.sap.hcp.cf.logging.sample.springboot.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 /**
  * This controller provides an endpoint to generate logs. This can be used to
@@ -16,6 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogController {
 
 	private static final String DEFAULT_LOG_MESSAGE = "This is the default log message!";
+	private Counter logsCount;
+	private Counter logsChars;
+
+	public LogController(@Autowired MeterRegistry meterRegistry) {
+		logsCount = Counter.builder("logs.generated.count").description("The number of generated log messages")
+				.register(meterRegistry);
+		logsChars = Counter.builder("logs.generated.chars").description("The number of generated characters")
+				.register(meterRegistry);
+	}
 
 	/**
 	 * Generate a log event with the given parameters.
@@ -35,20 +48,30 @@ public class LogController {
 		switch (logLevel.toLowerCase()) {
 		case "error":
 			logger.error(message);
+			logsCount.increment();
+			logsChars.increment(message.length());
 			return ResponseEntity.ok().body("Generated error log with message: \"" + message + "\".");
 		case "warn":
 		case "warning":
 			logger.warn(message);
+			logsCount.increment();
+			logsChars.increment(message.length());
 			return ResponseEntity.ok().body("Generated warn log with message: \"" + message + "\".");
 		case "info":
 		case "informational":
 			logger.info(message);
+			logsCount.increment();
+			logsChars.increment(message.length());
 			return ResponseEntity.ok().body("Generated info log with message: \"" + message + "\".");
 		case "debug":
 			logger.debug(message);
+			logsCount.increment();
+			logsChars.increment(message.length());
 			return ResponseEntity.ok().body("Generated debug log with message: \"" + message + "\".");
 		case "trace":
 			logger.trace(message);
+			logsCount.increment();
+			logsChars.increment(message.length());
 			return ResponseEntity.ok().body("Generated trace log with message: \"" + message + "\".");
 		}
 		return ResponseEntity.badRequest().body("Unknows log level \"" + logLevel + "\".");
