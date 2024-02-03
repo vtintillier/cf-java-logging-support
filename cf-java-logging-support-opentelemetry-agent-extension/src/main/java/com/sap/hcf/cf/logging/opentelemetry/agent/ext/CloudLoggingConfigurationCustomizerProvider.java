a45d83b944a559c73b4b577b3a0286e4f4c409d7
Karsten Schnitter <k.schnitter@sap.com>
2024-02-03 20:20:34 +0100

    Log OpenTelemetry Extension Version Number on StartUp
    Signed-off-by: Karsten Schnitter <k.schnitter@sap.com>

--- cf-java-logging-support-opentelemetry-agent-extension/src/main/java/com/sap/hcf/cf/logging/opentelemetry/agent/ext/CloudLoggingConfigurationCustomizerProvider.java
+++ cf-java-logging-support-opentelemetry-agent-extension/src/main/java/com/sap/hcf/cf/logging/opentelemetry/agent/ext/CloudLoggingConfigurationCustomizerProvider.java
@@ -5,12 +5,17 @@ import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
 import io.pivotal.cfenv.core.CfEnv;
 
+import java.util.logging.Logger;
+
 public class CloudLoggingConfigurationCustomizerProvider implements AutoConfigurationCustomizerProvider {
 
+    private static final Logger LOG = Logger.getLogger(CloudLoggingConfigurationCustomizerProvider.class.getName());
+    private static final String VERSION = "3.8.1";
     private static final CfEnv cfEnv = new CfEnv();
 
     @Override
     public void customize(AutoConfigurationCustomizer autoConfiguration) {
+        LOG.info("Initializing SAP BTP Observability extension " + VERSION);
         autoConfiguration
                 .addPropertiesSupplier(new CloudLoggingBindingPropertiesSupplier());
 
